d885d673b3fa087b6f342183858ae626a3e10c05
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-04-14 22:33:34 +0530

    Finished the base of the navbar

--- components/navbar.js
+++ components/navbar.js
@@ -6,22 +6,26 @@ import Image from 'next/dist/client/image'
 const Navbar = () => {
 	return (
 
-		<div className='navbar flex justify-between'>
-			
-			<div className='logo'>
-				<Image width={80} height={80} src='/logo.png' alt='' />
-			</div>
-			
-				<ul className='flexbox'>
-					<Link href='/'>Home</Link>
-					<Link href='/about'>About</Link>
-					<Link href='/blog'>Blog</Link>
-					<Link href='/contact'>Contact</Link>
-				</ul>
+		<div className='navbar'>
 
-			<div className='cart'>
-				<button>CART</button> 
+			<div className='logo'>
+
+				<Image width={80} height={80} src='/logo.png' alt='' />
+			
 			</div>
+
+			<div className='flexbox'>
+
+				<Link className= "NavLink" href='/'>Home</Link>
+				<Link className= "NavLink" href='/about'>About</Link>
+				<Link className= "NavLink" href='/blog'>Blog</Link>
+				<Link className= "NavLink" href='/contact'>Contact</Link>
+				
+				<button className='NavLink navbut'>CART</button>
+				
+			</div>
+
+
 		</div>
 
 	)
