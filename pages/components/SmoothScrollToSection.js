a6b96ccd996f58dbaa40741779b6f04e8be469b1
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-08-03 10:23:34 +0530

    Removed an obsolete file

--- pages/components/SmoothScrollToSection.js
+++
@@ -1,8 +0,0 @@
-const smoothScrollToSection = (id) => {
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-  
-  export default smoothScrollToSection;
